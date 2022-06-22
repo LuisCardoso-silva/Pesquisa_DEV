3a91250ff8755ed949e285c1d4e21fc0f56d0838
LuisCardoso-silva <luisgustavocrds0@gmail.com>
2022-06-22 08:19:56 -0300

    Projeto Iniciado.

---
+++ inicial.js
@@ -0,0 +1,41 @@
+/* Calculando área */
+
+/* var largura;
+var comprimento;
+var area;
+
+largura = 20;
+comprimento = 10;
+
+area = largura * comprimento;
+
+console.log('A área é de ' +area+ 'm²s'); */
+
+/* var cavalos;
+var ferraduras;
+
+cavalos = 20;
+ferraduras = cavalos * 4;
+
+console.log('A quantidade de ferraduras necessárias é: ' +ferraduras); */
+
+/* var pão;
+var broa;
+var tp;
+var tb;
+var poupança;
+
+pão = 0.12;
+broa = 1.50;
+tp = 500 * pão;
+tb = 250 * broa;
+poupança = ((tp + tb)/100) * 10;
+
+console.log('O valor arrecadado foi de R$ ' +(tp+tb)+ ', e o valor que deve ser guardado na poupança é de R$ ' + poupança) */
+
+/* var nome;
+var idade;
+nome = 'Gustavo';
+idade = 23;
+
+console.log(nome+ ', você já viveu ' +idade+ ' anos, e '+(idade*365)+ ' dias.'); */
